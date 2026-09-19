93a831215c1ba4998bb1fd3f8f67a2944bb54a5b
Eoul <eoul@local>
2026-09-19 14:47:18 +0900

    fix: require current control migration head

--- media_bridge_control/entrypoints.py
+++ media_bridge_control/entrypoints.py
@@ -13,7 +13,7 @@ from media_bridge_control.db import Database
 from media_bridge_control.runtime import build_control_runtime
 from media_bridge_control.settings import ControlSettings
 
-EXPECTED_MIGRATION_HEAD = "0003_deployment_auth"
+EXPECTED_MIGRATION_HEAD = "0005_routing_profiles"
 
 
 class MigrationStateError(RuntimeError):
